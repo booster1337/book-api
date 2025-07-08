e745e66defa97af126cf70ec53f8f70a784a65a2
booster1337 <miloszkolakowski@wp.pl>
2025-07-08 13:55:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,59 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello(): return 'Book API działa'
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-if __name__ == '__main__': app.run()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+# Model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author = db.Column(db.String(100))
+
+# Inicjalizacja bazy (jednorazowo)
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# GET /books
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    return jsonify([{'id': b.id, 'title': b.title, 'author': b.author} for b in books])
+
+# GET /books/<id>
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify({'id': book.id, 'title': book.title, 'author': book.author})
+
+# POST /books
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    new_book = Book(title=data['title'], author=data['author'])
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({'message': 'Dodano książkę!'}), 201
+
+# PUT /books/<id>
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+    data = request.get_json()
+    book.title = data.get('title', book.title)
+    book.author = data.get('author', book.author)
+    db.session.commit()
+    return jsonify({'message': 'Zaktualizowano książkę'})
+
+# DELETE /books/<id>
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'message': 'Usunięto książkę'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
